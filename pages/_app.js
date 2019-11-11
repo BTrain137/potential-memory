400fb69ab3faa67830a69c20ed9905cc00deddfe
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-11-11 10:40:55 -0800

    Revert back to an older _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,22 +3,14 @@ import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/styles.css';
+import translations from '@shopify/polaris/locales/en.json';
 import Cookies from 'js-cookie';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
 
-const client = new ApolloClient({
-  fetchOptions: {
-    credentials: 'include',
-  },
-});
 
 class MyApp extends App {
-
   render() {
     const { Component, pageProps } = this.props;
     const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
-
     return (
       <React.Fragment>
         <Head>
@@ -26,10 +18,8 @@ class MyApp extends App {
           <meta charSet="utf-8" />
         </Head>
         <Provider config={config}>
-          <AppProvider>
-            <ApolloProvider client={client}>
-              <Component {...pageProps} />
-            </ApolloProvider>
+          <AppProvider i18n={translations}>
+            <Component {...pageProps} />
           </AppProvider>
         </Provider>
       </React.Fragment>
